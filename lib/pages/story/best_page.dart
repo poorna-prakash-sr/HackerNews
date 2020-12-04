1754e7aea984b5205ebcdfa2e0e97e742d552694
SR Prakash <poornaprakash.it19@bitsathy.ac.in>
2020-12-04 11:04:40 +0530

    commit

--- lib/pages/story/best_page.dart
+++ lib/pages/story/best_page.dart
@@ -92,6 +92,7 @@ class _BestPageState extends State<BestPage> {
                         ),
                       ),
                       
+                      
                     ],
                   )
                 ],
